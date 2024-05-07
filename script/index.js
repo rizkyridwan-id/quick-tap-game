cb6c01c4f16949a9320bab01a4eab7630d6224f1
riz <mr.ridwan.thekeeper@gmail.com>
2024-05-07 09:46:58 +0700

    initial commit

---
+++ script/index.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+/**
+ * CANVAS SETUP
+ */
+ctx.width = window.innerWidth
+ctx.height = window.innerHeight
+
+/**
+ * GLOBAL VARS
+ */
+
+
+/**
+ * USER DEFINED FUNCTIONS
+ */
